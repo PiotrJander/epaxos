f77570f70807469e39d2f37eb32143a2bd360c24
Piotr Jander <pjander2@gmail.com>
2019-12-23 14:44:26 +0100

    More basic setup

--- src/server_piotr.rs
+++ src/server_piotr.rs
@@ -16,33 +16,65 @@ use std::{
 };
 use grpc::rt::ServerServiceDefinition;
 
-pub const QUORUM: u16 = 2;
-pub const REPLICAS_NUM: u16 = 3;
-pub const REPLICA1_PORT: u16 = 10000;
-pub const REPLICA2_PORT: u16 = 10001;
-pub const REPLICA3_PORT: u16 = 10002;
-pub const REPLICA4_PORT: u16 = 10003;
-pub const REPLICA5_PORT: u16 = 10004;
-pub const LOCALHOST: &str = "localhost";
+const QUORUM: u32 = 3;
+const REPLICAS_NUM: u32 = 5;
+const LOCALHOST: &str = "localhost";
+static REPLICA_INTERNAL_PORTS: &'static [u16] = &[10000, 10001, 10002, 10003, 10004];
+static REPLICA_EXTERNAL_PORTS: &'static [u16] = &[10000, 10001, 10002, 10003, 10004];
+
+struct InstanceId(u32);
+
+enum CommandState {
+    PreAccepted,
+    Accepted,
+    Committed
+}
 
 struct Command {
     key: String,
     value: i32,
+    seq: u32,
+    dependencies: Vec<Instance>,
+    state: CommandState
 }
 
 // TODO do we risk deadlocks by having mutexes and cloning?
 #[derive(Clone)]
 struct Epaxos {
-    id: u32,
+    id: Arc<Mutex<InstanceId>>,
     //    store: Arc<Mutex<HashMap<String, i32>>>,
     commands: Arc<Mutex<Vec<Vec<Command>>>>,
     instance_number: Arc<Mutex<u32>>,
-    replicas: Arc<Mutex<Vec<InternalClient>>>,
+    replicas: Arc<Mutex<HashMap<InstanceId, InternalClient>>>,
 }
 
 impl Epaxos {
-    fn new(id: u32) -> Epaxos {
+
+    fn new(id: InstanceId) -> Epaxos {
+
         let mut commands = Vec::new();
+        let mut replicas = HashMap::new();
+
+        for i in 0..REPLICAS_NUM {
+            commands.push(Vec::new());
+
+            if i != id.0 {
+                let internal_client =
+                    grpc::Client::new_plain(LOCALHOST, REPLICA_INTERNAL_PORTS[i], Default::default()).unwrap();
+                let replica = InternalClient::with_client(grpc_replica1);
+                replicas.insert(InstanceId(i), replica)
+            }
+        }
+
+        return Epaxos {
+            id: Arc::new(Mutex::new(id)),
+            commands: Arc::new(Mutex::new(commands)),
+            instance_number: Arc::new(Mutex::new(0)),
+            replicas: Arc::new(Mutex::new(replicas)),
+        };
+    }
+
+    fn consensus(&self, write_req: &WriteRequest) {
         unimplemented!()
     }
 }
@@ -63,7 +95,10 @@ impl Internal for Epaxos {
 
 impl External for Epaxos {
     fn write(&self, o: RequestOptions, p: WriteRequest) -> SingleResponse<WriteResponse> {
-        unimplemented!()
+        self.consensus(&p);
+        let mut r = WriteResponse::new();
+        r.set_commit(true);
+        grpc::SingleResponse::completed(r)
     }
 
     fn read(&self, o: RequestOptions, p: ReadRequest) -> SingleResponse<ReadResponse> {
@@ -82,11 +117,11 @@ fn start_server(service: ServerServiceDefinition, port: u16) -> () {
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let id: u32 = args[1].parse().unwrap();
-    let internal_port: u16 = args[2].parse().unwrap();
-    let external_port: u16 = args[3].parse().unwrap();
+    let id = args[1].parse().unwrap();
+    let internal_port = REPLICA_INTERNAL_PORTS[id];
+    let external_port = REPLICA_EXTERNAL_PORTS[id];
 
-    let epaxos = Epaxos::new(id);
+    let epaxos = Epaxos::new(InstanceId(id));
     start_server(InternalServer::new_service_def(epaxos), internal_port);
     start_server(ExternalServer::new_service_def(epaxos.clone()), external_port);
 
